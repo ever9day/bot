30463541196185180cec6cba93c6787d73b3025d
ever9day <37009736+ever9day@users.noreply.github.com>
2018-03-24 11:23:00 +0600

    Bots
    It bot

---
+++ botexe.py
@@ -0,0 +1,22 @@
+import telebot
+import Funksii
+
+token = '544257192:AAG5Z42_l4QfXgfOBC-5kI4KN0Kt_xSOzG8'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def check_message(message):
+    if message.text == "писок студентов":
+        for student in Funksii.getListStudents():
+            bot.send_message(message.chat.id, student[0])
+    elif message.text == 'Сред. оценка':
+        bot.send_message(message.chat.id, Funksii.getAverageMarks())
+    elif message.text.isdigit():
+        bot.send_message(message.chat.id,Funksii.getByIndex(int(message.text))[0])
+        bot.send_message(message.chat.id,Funksii.getByIndex(int(message.text))[1])
+    else:
+        bot.send_message(message.chat.id, "Такой команды нет")
+
+        
+bot.polling(none_stop=True)
